e0fc4a61305644a0f71a8964f086a029c661f8d3
Bandy <pemisin.toyo@gmail.com>
2021-03-23 05:45:14 -0400

    Added all components

---
+++ ravenous/src/components/BusinessList/BusinessList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './BusinessList.css';
+import Business from '../Business/Business';
+
+// Business List Component
+class BusinessList extends React.Component{
+    render(){
+        return(
+            <div className="BusinessList">
+                <Business />
+                <Business />
+                <Business />
+                <Business />
+                <Business />
+                <Business />
+            </div> 
+        );   
+    }
+
+
+}
+export default BusinessList;
+
